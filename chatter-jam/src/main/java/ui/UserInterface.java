276ff8aa692222d500eb74f97dddf1cf7e9d3a90
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-20 16:53:38 -0500

    Created user interface method to display the home screen

--- chatter-jam/src/main/java/ui/UserInterface.java
+++ chatter-jam/src/main/java/ui/UserInterface.java
@@ -1,4 +1,21 @@
 package ui;
 
+import utils.Utils;
+
 public class UserInterface {
+
+	public int displayHomeScreen() {
+		System.out.println("""
+							_________________MAIN MENU_________________
+				
+						---OPTIONS---
+				1 - View All Stories
+				2 - Create a New Story
+				3 - Admin
+				0 - Exit
+				""");
+
+		return Utils.getUserInputIntMinMax("Enter here:\n", 0, 5);
+	}
+
 }
