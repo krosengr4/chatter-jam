9c98769b50209f42cd5ba29c661d8dcbfb5b9848
krosengr4 <rosenkev4@gmail.com>
2025-08-19 22:29:40 -0500

    Added the database queries into the project

---
+++ chatter-jam/Database/chatterjams-queries.sql
@@ -0,0 +1,33 @@
+# ---------------------------------------------------------------------- #
+# Target DBMS:           MySQL                                           #
+# Project name:          ChatterJams                               #
+# ---------------------------------------------------------------------- #
+DROP DATABASE IF EXISTS chatterjams;
+
+CREATE DATABASE IF NOT EXISTS chatterjams;
+
+USE chatterjams;
+
+# ---------------------------------------------------------------------- #
+# Tables                                                                 #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE posts (
+post_id INT NOT NULL AUTO_INCREMENT,
+title varchar(255) NOT NULL,
+content TEXT NOT NULL,
+author varchar(75), 
+date_posted DATETIME NOT NULL,
+PRIMARY KEY (post_id)
+);
+
+CREATE TABLE comments (
+comment_id INT AUTO_INCREMENT NOT NULL,
+post_id INT NOT NULL,
+content TEXT NOT NULL,
+author varchar(75),
+date_posted DATETIME NOT NULL,
+PRIMARY KEY (comment_id),
+FOREIGN KEY (post_id) REFERENCES posts(post_id)
+);
+ 
