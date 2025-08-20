99198d156c3ca3475ba0058d058639bc018ab916
krosengr4 <rosenkev4@gmail.com>
2025-08-19 22:22:53 -0500

    Added a title screen to the app

--- chatter-jam/src/main/java/Main.java
+++ chatter-jam/src/main/java/Main.java
@@ -1,2 +1,15 @@
 public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("""
+
+
+										WELCOME TO CHATTER JAMS!!!
+				______________________________________________________________________
+					Share your experiences, stories, and ideas and interact with others!
+				_______________________________________________________________________
+				
+				""");
+	}
+
 }
