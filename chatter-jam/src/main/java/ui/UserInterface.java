45b34debae000eeea6aeb79808987c1e494453f5
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-21 05:13:18 -0500

    Created UI method to display the Posts screen

--- chatter-jam/src/main/java/ui/UserInterface.java
+++ chatter-jam/src/main/java/ui/UserInterface.java
@@ -9,13 +9,25 @@ public class UserInterface {
 							_________________MAIN MENU_________________
 				
 						---OPTIONS---
-				1 - View All Posts
+				1 - Go to posts screen
 				2 - Create a New Post
-				3 - Admin
 				0 - Exit
 				""");
 
-		return Utils.getUserInputIntMinMax("Enter here:\n", 0, 5);
+		return Utils.getUserInputIntMinMax("Enter here:\n", 0, 2);
+	}
+
+	public int displayPostScreen() {
+		System.out.println("""
+					_________________POSTS SCREEN_________________
+				
+				---OPTIONS---
+		1 - See all posts
+		2 - See all posts from a user
+		3 - See post by id (here you can leave a comment)
+		""");
+
+		return Utils.getUserInputIntMinMax("Enter here:\n", 0, 3);
 	}
 
 }
