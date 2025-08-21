851d4402fd739227db2fa62dc09c34bdcf33d27d
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-21 07:42:58 -0500

    Created method to map a result row to a comment object and return it

--- chatter-jam/src/main/java/data/mysql/MySqlCommentDao.java
+++ chatter-jam/src/main/java/data/mysql/MySqlCommentDao.java
@@ -4,6 +4,9 @@ import data.CommentDao;
 import models.Comment;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +14,17 @@ public class MySqlCommentDao extends MySqlBaseDao implements CommentDao {
 
 	public MySqlCommentDao(DataSource dataSource) {
 		super(dataSource);
+	}
+
+
+	private Comment mapRow(ResultSet result) throws SQLException {
+		int commentId = result.getInt("comment_id");
+		int postId = result.getInt("post_id");
+		String content = result.getString("content");
+		String author = result.getString("author");
+		LocalDate datePosted = result.getTimestamp("date_posted").toLocalDateTime().toLocalDate();
+
+		return new Comment(commentId, postId, content, author, datePosted);
 	}
 
 }
