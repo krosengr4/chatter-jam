5712dfec08818748538152cd09c4539d53591389
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-21 05:23:11 -0500

    Coded the configuration class for the database

--- chatter-jam/src/main/java/config/DatabaseConfiguration.java
+++ chatter-jam/src/main/java/config/DatabaseConfiguration.java
@@ -1,4 +1,16 @@
 package config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 public class DatabaseConfiguration {
+
+	private static final BasicDataSource dataSource = new BasicDataSource();
+
+	public static BasicDataSource setDataSource() {
+		dataSource.setUrl("jdbc:mysql://localhost:3306/chatterjams");
+		dataSource.setUsername("root");
+		dataSource.setPassword(System.getenv("SQL_PASSWORD"));
+		return dataSource;
+	}
+
 }
