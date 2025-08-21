b7760d8f27e7438bb1e971b91f52a2be44e32331
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-21 05:47:24 -0500

    Created method in PostDao to get all the posts

--- chatter-jam/src/main/java/data/mysql/MySqlPostDao.java
+++ chatter-jam/src/main/java/data/mysql/MySqlPostDao.java
@@ -6,9 +6,13 @@ import models.Post;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MySqlPostDao extends MySqlBaseDao implements PostDao {
 
@@ -16,6 +20,26 @@ public class MySqlPostDao extends MySqlBaseDao implements PostDao {
 
 	public MySqlPostDao(DataSource dataSource) {
 		super(dataSource);
+	}
+
+	@Override
+	public List<Post> getAll() {
+		List<Post> posts = new ArrayList<>();
+		String query = "SELECT * FROM posts;";
+
+		try(Connection connection = getConnection()) {
+			PreparedStatement statement = connection.prepareStatement(query);
+
+			ResultSet results = statement.executeQuery();
+			while(results.next()) {
+				posts.add(mapRow(results));
+			}
+
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+		return posts;
 	}
 
 
