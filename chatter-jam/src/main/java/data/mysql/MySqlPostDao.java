5e0b83edd62741fe9b072ffd318e87f79cad2be3
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-21 05:39:37 -0500

    Created method that maps a result row from the db to a Post object

--- chatter-jam/src/main/java/data/mysql/MySqlPostDao.java
+++ chatter-jam/src/main/java/data/mysql/MySqlPostDao.java
@@ -2,9 +2,13 @@ package data.mysql;
 
 import config.DatabaseConfiguration;
 import data.PostDao;
+import models.Post;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class MySqlPostDao extends MySqlBaseDao implements PostDao {
 
@@ -12,6 +16,17 @@ public class MySqlPostDao extends MySqlBaseDao implements PostDao {
 
 	public MySqlPostDao(DataSource dataSource) {
 		super(dataSource);
+	}
+
+
+	private Post mapRow(ResultSet result) throws SQLException {
+		int postId = result.getInt("post_id");
+		String title = result.getString("title");
+		String content = result.getString("content");
+		String author = result.getString("author");
+		LocalDate datePosted = result.getTimestamp("date_posted").toLocalDateTime().toLocalDate();
+
+		return new Post(postId, title, content, author, datePosted);
 	}
 
 }
